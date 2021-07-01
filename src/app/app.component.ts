3db46d1eac2e0afb937b17b72ee6c6bd3fb7d866
Christine <teenahshyro@gmail.com>
2021-07-01 11:30:05 +0300

    initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFlash } from './flash.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flashcard';
+  editing = false;
+  editingId: number;
+
+  flashs:IFlash[] = [
+    {
+    question: 'Question 1',
+    answer: 'Answer 1',
+    show: false,
+    id:this.getRandomNumber()
+  },
+  {
+    question: 'Question 2',
+    answer: 'Answer 2',
+    show: false,
+    id:this.getRandomNumber()
+  },
+  {
+    question:'Question 3',
+    answer: 'Answer 3',
+    show: false,
+    id:this.getRandomNumber()
+  }
+]
+
+  getRandomNumber(){
+    return Math.floor(Math.random() * 10000)
+  }
+
+  trackByFlashId(index,flash){
+    return flash.id
+  }
+
+  handleToggleCard(id: number) {
+    const flash = this.flashs.find(flash => flash.id === id);
+    flash.show = !flash.show;
+  }
+  // handleDelete(id: number) {
+  //   const flashId = this.flashs.indexOf(flash => flash.id ===
+  //     id);
+  //   this.flashs.splice(flashId, 1)
+  // }
+
+
+  handleEdit(id: number) {
+    this.editing = true;
+    this.editingId = id;
+    // TODO: We will add editing logic after adding the form
+  }
+
 }
